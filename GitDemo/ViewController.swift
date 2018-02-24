1758d48d541be3e1c576e062ec0224086af0d402
user <user@users-MacBook-Pro.local>
2018-02-24 09:49:22 +0300

    my first commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by user on 2/23/18.
+//  Copyright © 2018 Varfolomeew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      print("Another print statement")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
